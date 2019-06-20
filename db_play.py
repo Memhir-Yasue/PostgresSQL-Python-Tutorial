9944d1f818bbac7a744006c9a0ae17089bd8fb52
Memhir-YasuE <habesha787@gmail.com>
2019-06-20 16:10:32 -0400

    Insertion

--- db_play.py
+++ db_play.py
@@ -42,6 +42,14 @@ def connect():
             conn.close()
             print('Database connection closed!')
 
+"""
+The purpose of this tutorial is to connect to create tables for the PostgreSQL database server"
+
+Step 1: CREATE TABLE table_name
+Step 2: process creatin in Python
+Step 3: Connect to DB if not connected
+
+"""
 def create_tables():
     """ Create tables in the PostgreSQL database """
     commands = (
@@ -101,5 +109,64 @@ def create_tables():
         if conn is not None:
             conn.close()
 
+"""
+The purpose of this tutorial is to insert data into the vendors table of our Database
+We'll be inserting a single entry (record) as well as a list containing multiple records
+
+Step 1: Connect to DB if not connected
+"""
+
+def insert_vendor(vendor_name):
+    """ insert a new vendor into the vendor table """
+    sql = """INSERT INTO vendors(vendor_name) VALUES(%s) RETURNING vendor_id;"""
+    conn = None
+    vendor_id = None
+    try:
+        # read database configration
+        params = config()
+        conn = psycopg2.connect(**params)
+        # create new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (vendor_name,))
+        # get the generated id back
+        vendor_id = cur.fetchone()[0]
+        # commit changes to the Database
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    return vendor_id
+
+def insert_vendor_list(vendor_list):
+    sql = "INSERT INTO vendors(vendor_name) VALUES(%s)"
+    conn =  None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute SQL INSERT statement
+        cur.executemany(sql, vendor_list)
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
 if __name__ == '__main__':
-    create_tables()
+    # insert one vendor
+    insert_vendor("Selamta Honey")
+    insert_vendor_list([
+        ('Makeda Inc.',),
+        ('Yesus Yadinal Ltd.',),
+        ('Dalhak Industries Ltd.',),
+    ])
